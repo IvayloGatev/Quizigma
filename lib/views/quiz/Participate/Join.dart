e5c96a9ffc52f2d94c3335597d709daa07465605
yatharthupmanue <yathuyatharth123@gmail.com>
2020-12-03 11:28:02 +0100

    in progress, Join()

--- lib/views/quiz/Participate/Join.dart
+++ lib/views/quiz/Participate/Join.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+//import './quiz_controller';
 
 class Join extends StatelessWidget {
+   //final _controller = QuizController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -9,6 +11,47 @@ class Join extends StatelessWidget {
         backgroundColor: Colors.purple,
         title: Text("Welcome to your Quiz"),
       ),
+      body: Center(
+        child: MyStatefulWidget(),
+      ),
     );
   }
 }
+/// This is the stateful widget that the main application instantiates.
+class MyStatefulWidget extends StatefulWidget {
+  MyStatefulWidget({Key key}) : super(key: key);
+
+  @override
+  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
+}
+
+/// This is the private State class that goes with MyStatefulWidget.
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  String dropdownValue = 'One';
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButton<String>(
+      value: dropdownValue,
+      icon: Icon(Icons.arrow_downward),
+      iconSize: 24,
+      elevation: 16,
+      style: TextStyle(color: Colors.deepPurple),
+      underline: Container(
+        height: 2,
+        color: Colors.deepPurpleAccent,
+      ),
+      onChanged: (String newValue) {
+        setState(() {
+          dropdownValue = newValue;
+        });
+      },
+      items: <String>['One', 'Two', 'Three', 'Four', 'Five']
+          .map<DropdownMenuItem<String>>((String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Text(value),
+        );
+      }).toList(),
+    );
+  }}
