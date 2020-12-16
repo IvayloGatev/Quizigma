89753cb019f9784cb785c05ba25c0e9529591351
sevdeyanik <72306627+sevdeyanik@users.noreply.github.com>
2020-12-16 19:03:33 +0100

    Update join.dart

--- lib/views/quiz/Participate/join.dart
+++ lib/views/quiz/Participate/join.dart
@@ -8,14 +8,39 @@ import 'package:quizigma/views/quiz/Participate/quiz_list.dart';
 
 class Join extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
+  /*Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.purple[50],
       appBar: AppBar(
         backgroundColor: Colors.purple,
         title: Text("Welcome to your Quiz"),
       ),
-      body: QuizList(),
+      body: BrewList(),
+    );
+  }*/
+
+  Widget build(BuildContext context) {
+    return StreamProvider<List<Quiz>>.value(
+      value: QuizController().database.quizes,
+      child: Scaffold(
+        backgroundColor: Colors.purple[50],
+        appBar: AppBar(
+          title: Text('Welcome to your Quiz'),
+          backgroundColor: Colors.purple[200],
+          elevation: 0.0,
+          /*actions: <Widget>[
+            FlatButton.icon(
+              icon: Icon(Icons.person),
+              label: Text('logout'),
+              onPressed: () {
+                Text('logout opt must come here?');
+              },
+            ),
+          ],
+        */
+        ),
+        body: QuizList(),
+      ),
     );
   }
 }
