8fe6447246dc4d4269d2b2f457ba46a0a08d0bd8
sevdeyanik <72306627+sevdeyanik@users.noreply.github.com>
2020-12-16 21:49:06 +0100

    Update quiz.dart

--- lib/models/quiz.dart
+++ lib/models/quiz.dart
@@ -4,29 +4,24 @@ import 'package:random_string/random_string.dart';
 class Quiz {
   static final int minQuestions = 5;
 
-  String id;
-  String category;
-  String name;
+  String _id;
+  String _category;
+  String _name;
   List<Question> _questions;
   DateTime _startDate;
   DateTime _endDate;
 
-  Quiz.namedconstructor({this.id, this.category, this.name});
-
   // Constructors for the class Quiz.
-  Quiz(String category, String name,
-      [List<Question> questions,
-      DateTime startDate,
-      DateTime endDate,
-      String id]) {
+  Quiz(String category, String name, List<Question> questions,
+      [DateTime startDate, DateTime endDate, String id]) {
     if (questions.length < minQuestions) {
       throw new Exception();
     }
 
     if (id == null) {
-      id = randomAlphaNumeric(8);
+      _id = randomAlphaNumeric(8);
     } else {
-      id = id;
+      _id = id;
     }
 
     if (startDate == null) {
@@ -41,12 +36,23 @@ class Quiz {
       _endDate = endDate;
     }
 
-    category = category;
-    name = name;
+    _category = category;
+    _name = name;
     _questions = questions;
   }
 
   // Getters for the properties.
+  String get id {
+    return _id;
+  }
+
+  String get category {
+    return _category;
+  }
+
+  String get name {
+    return _name;
+  }
 
   List<Question> get questions {
     return _questions;
