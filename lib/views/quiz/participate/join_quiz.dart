9200ffccf84aac1ad40b08a805f72a3f8eefd2d2
Andreas Braz <45979982+Andreas-Braz@users.noreply.github.com>
2020-12-20 19:41:39 +0100

    Update join_quiz.dart

--- lib/views/quiz/participate/join_quiz.dart
+++ lib/views/quiz/participate/join_quiz.dart
@@ -2,22 +2,30 @@ import 'package:flutter/material.dart';
 import 'package:quizigma/controllers/quiz_controller.dart';
 import 'package:provider/provider.dart';
 import 'package:quizigma/models/quiz.dart';
-import 'package:quizigma/views/quiz/Participate/quiz_list.dart';
+import 'package:quizigma/views/quiz/participate/quiz_list.dart';
+import 'package:quizigma/views/quiz/participate/join_questions.dart';
 
 class JoinQuiz extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return StreamProvider<List<Quiz>>.value(
-      value: QuizController().database.quizes,
-      child: Scaffold(
-        backgroundColor: Colors.purple[50],
-        appBar: AppBar(
-          title: Text('Welcome to your Quiz'),
-          backgroundColor: Colors.purple[200],
-          elevation: 0.0,
+    return FlatButton(
+      padding: const EdgeInsets.all(15),
+      child: StreamProvider<List<Quiz>>.value(
+        value: QuizController().database.quizes,
+        child: Scaffold(
+          backgroundColor: Colors.purple[50],
+          appBar: AppBar(
+            title: Text('Welcome to your Quiz'),
+            backgroundColor: Colors.purple[200],
+            elevation: 0.0,
+          ),
+          body: QuizList(),
         ),
-        body: QuizList(),
       ),
+      onPressed: () async {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => JoinQuestions()));
+      },
     );
   }
 }
