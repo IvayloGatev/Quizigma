d63162045a66261bd3688f0a40510d9bf6b770a5
yatharthupmanue <yathuyatharth123@gmail.com>
2020-12-25 16:43:37 +0100

    creator layout in progress

--- lib/views/quiz/creator/creator.dart
+++ lib/views/quiz/creator/creator.dart
@@ -13,15 +13,20 @@ class QuizCreator extends StatefulWidget {
 
 //Note to backend: You can retrieve the information from answersList of _MyFormState class.
 class _QuizCreatorState extends State<QuizCreator> {
-  final _formKey = GlobalKey<FormState>();
-  final _controller = QuizController();
-
-  List<QuestionTextEditor> editors = List<QuestionTextEditor>();
-  TextEditingController nameEditingController = TextEditingController();
+  List<int> bottom = [0];
+  List<String> categories = [
+    'Literature',
+    'History',
+    'Physics',
+    'Chemistry',
+    'Biology',
+    'General Knowledge',
+    'Polictics',
+    'Nature'
+  ];
 
   String category = 'Literature';
   int dropdownValue = 15;
-
   List<int> dropDownValues = [
     15,
     16,
@@ -55,18 +60,25 @@ class _QuizCreatorState extends State<QuizCreator> {
     44,
     45
   ];
-  List<String> categories = [
-    'Literature',
-    'History',
-    'Physics',
-    'Chemistry',
-    'Biology',
-    'General Knowledge',
-    'Polictics',
-    'Nature'
-  ];
+
+  List<QuestionTextEditor> editors = List<QuestionTextEditor>();
+  TextEditingController nameEditingController = TextEditingController();
+  List<int> top = [];
+
+  final _controller = QuizController();
+  final _formKey = GlobalKey<FormState>();
+
+  void _showCreatorAlert() {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return CreatorAlert();
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
+    const Key centerKey = ValueKey('bottom-sliver-list');
     return Scaffold(
         backgroundColor: Colors.grey[200],
         appBar: AppBar(
@@ -78,122 +90,147 @@ class _QuizCreatorState extends State<QuizCreator> {
             onPressed: () => Navigator.pop(context, true),
           ),
         ),
-        body: Form(
-            key: _formKey,
+        body: SingleChildScrollView(
             child: Column(children: [
-              Padding(
-                padding: const EdgeInsets.only(right: 32.0),
-                child: TextFormField(
-                  controller: nameEditingController,
-                  decoration: InputDecoration(hintText: 'Enter quiz name'),
-                  validator: (v) {
-                    if (v.trim().isEmpty) return 'Please enter something';
-                    return null;
-                  },
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(right: 32.0),
-                child: DropdownButtonFormField<String>(
-                  value: category,
-                  icon: Icon(Icons.arrow_downward),
-                  iconSize: 16,
-                  elevation: 16,
-                  decoration: InputDecoration(
-                    labelText: 'Select category',
-                    labelStyle: TextStyle(
-                      fontSize: 22,
-                    ),
+          Form(
+              key: _formKey,
+              child: Column(children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+                  child: TextFormField(
+                    controller: nameEditingController,
+                    decoration: InputDecoration(hintText: 'Enter quiz name'),
+                    validator: (v) {
+                      if (v.trim().isEmpty) return 'Please enter something';
+                      return null;
+                    },
                   ),
-                  onChanged: (String value) {
-                    setState(() {
-                      category = value;
-                    });
-                  },
-                  items: categories.map((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    );
-                  }).toList(),
                 ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(right: 32.0),
-                child: DropdownButtonFormField<int>(
-                  value: dropdownValue,
-                  icon: Icon(Icons.arrow_downward),
-                  iconSize: 16,
-                  elevation: 16,
-                  decoration: InputDecoration(
-                    labelText: 'Select question timer',
-                    labelStyle: TextStyle(
-                      fontSize: 22,
+                SizedBox(
+                  height: 10,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+                  child: DropdownButtonFormField<String>(
+                    value: category,
+                    icon: Icon(Icons.arrow_downward),
+                    iconSize: 16,
+                    elevation: 16,
+                    decoration: InputDecoration(
+                      labelText: 'Select category',
+                      labelStyle: TextStyle(
+                        fontSize: 22,
+                      ),
                     ),
+                    onChanged: (String value) {
+                      setState(() {
+                        category = value;
+                      });
+                    },
+                    items: categories.map((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
                   ),
-                  onChanged: (int newValue) {
-                    setState(() {
-                      dropdownValue = newValue;
-                    });
-                  },
-                  items: dropDownValues.map<DropdownMenuItem<int>>((int value) {
-                    return DropdownMenuItem<int>(
-                      value: value,
-                      child: Text(value.toString()),
-                    );
-                  }).toList(),
                 ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Expanded(
-                child: QuestionList(editors: editors),
-              ),
-              RaisedButton(
-                onPressed: () {
-                  if (_formKey.currentState.validate()) {
-                    String name = nameEditingController.text;
-                    List<Question> questions = List<Question>();
-                    int value = dropdownValue;
-                    for (var editor in editors) {
-                      String questionText = editor.questionTextController.text;
-                      List<String> answers = List<String>();
-                      for (var answerTextController
-                          in editor.answerTextControllers) {
-                        answers.add(answerTextController.text);
+                SizedBox(height: 10),
+                Padding(
+                  padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+                  child: DropdownButtonFormField<int>(
+                    value: dropdownValue,
+                    icon: Icon(Icons.arrow_downward),
+                    iconSize: 16,
+                    elevation: 16,
+                    decoration: InputDecoration(
+                      labelText: 'Select question timer',
+                      labelStyle: TextStyle(
+                        fontSize: 22,
+                      ),
+                    ),
+                    onChanged: (int newValue) {
+                      setState(() {
+                        dropdownValue = newValue;
+                      });
+                    },
+                    items:
+                        dropDownValues.map<DropdownMenuItem<int>>((int value) {
+                      return DropdownMenuItem<int>(
+                        value: value,
+                        child: Text(value.toString()),
+                      );
+                    }).toList(),
+                  ),
+                ),
+                SizedBox(height: 10),
+                Padding(
+                  padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+                  child: DropdownButtonFormField<int>(
+                    value: dropdownValue,
+                    icon: Icon(Icons.arrow_downward),
+                    iconSize: 16,
+                    elevation: 16,
+                    decoration: InputDecoration(
+                      labelText: 'Select question timer',
+                      labelStyle: TextStyle(
+                        fontSize: 22,
+                      ),
+                    ),
+                    onChanged: (int newValue) {
+                      setState(() {
+                        dropdownValue = newValue;
+                      });
+                    },
+                    items:
+                        dropDownValues.map<DropdownMenuItem<int>>((int value) {
+                      return DropdownMenuItem<int>(
+                        value: value,
+                        child: Text(value.toString()),
+                      );
+                    }).toList(),
+                  ),
+                ),
+                SizedBox(height: 10),
+                /*Expanded(
+                  child: QuestionList(editors: editors),
+                ),  
+                RaisedButton(
+                  onPressed: () {
+                    if (_formKey.currentState.validate()) {
+                      String name = nameEditingController.text;
+                      List<Question> questions = List<Question>();
+                      int value = dropdownValue;
+                      for (var editor in editors) {
+                        String questionText =
+                            editor.questionTextController.text;
+                        List<String> answers = List<String>();
+                        for (var answerTextController
+                            in editor.answerTextControllers) {
+                          answers.add(answerTextController.text);
+                        }
+                        questions.add(Question(
+                            questions.length + 1,
+                            questionText,
+                            answers,
+                            editor.correctAnswer,
+                            value));
                       }
-                      questions.add(Question(questions.length + 1, questionText,
-                          answers, editor.correctAnswer, value));
+
+                      Quiz quiz = Quiz(category, name, questions);
+                      _controller.addQuiz(quiz);
+                      _showCreatorAlert();
                     }
-
-                    Quiz quiz = Quiz(category, name, questions);
-                    _controller.addQuiz(quiz);
-                    _showCreatorAlert();
-                  }
-                },
-                padding: const EdgeInsets.symmetric(
-                    horizontal: 16.0, vertical: 20.0),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-                child: Text('Submit'),
-                color: Colors.deepPurple,
-              ),
-            ])));
-  }
-
-  void _showCreatorAlert() {
-    showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return CreatorAlert();
-        });
+                  },
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 16.0, vertical: 20.0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  child: Text('Submit'),
+                  color: Colors.deepPurple,
+                ), */
+              ])),
+        ])));
   }
 }
